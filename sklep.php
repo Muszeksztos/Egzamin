6b03a68a3f318eb1b7210bff400624ac883e2e08
Muszeksztos <90259503+Muszeksztos@users.noreply.github.com>
2022-11-28 08:45:49 +0100

    Add files via upload

---
+++ sklep.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Warzywniak</title>
+</head>
+<body>
+    
+<header class="lewy">
+		<h1> Internetowy sklep z eko-warzywami</h1>
+	</header>
+	<header class="prawy">
+		<ol>
+			<li> warzywa </li>
+			<li> owoce </li>
+			<li> <a href="https://terapiasokami.pl/" > Soki </a></li>
+		</ol>
+	</header>
+<main>
+
+<div class="skrypt1">
+    <h5>Tu mial byc skrypt</h5>
+</div>
+
+</main>
+<footer>
+		<form method="post">
+		Nazwa:<input type="text" name="nazwa" id="nazwa">
+		Cena:<input type="text" name="cena" id="cena">
+		<input type="submit" name="przeslij" id="przeslij" value="Dodaj produkt">
+		</form>
+
+		<p>Stronę wykonał: Michał Mucha Pesel: 03*********</p>
+	</footer>
+</body>
+</html>
